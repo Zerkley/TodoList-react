504cd043f6c8650aa833cca5bca454ef63114939
Zerkley <miguelmr94@gmail.com>
2023-06-23 12:03:33 +0100

    V1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [toDo, setToDo] = useState("");
+  const [tareas, setTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTareas(tareas.concat(toDo)); // darle valor introducido al array
+    console.log(toDo);
+    // añadir ese valor a la lista
+    setToDo(""); // resetear el valor
+  };
+
+  //   const removeItem = () => {
+  //     setList((current) => current.filter((tarea) => fruit.index !== index));
+  //   };
+  console.log(tareas);
+  return (
+    <div className="container my-5 d-flex justify-content-center">
+      <form action="" onSubmit={handleSubmit} id="form">
+        <div>
+          <input
+            type="text"
+            onChange={(e) => {
+              setToDo(e.target.value);
+            }}
+          />
+          <button type="submit">dale</button>
+        </div>
+        <div>
+          <ul>
+            {tareas
+              ? tareas.map((tarea, index) => (
+                  <li key={index}>
+                    {tarea}
+                    <button type="button" onClick={() => removeItem(index)}>
+                      X
+                    </button>
+                  </li>
+                ))
+              : null}
+          </ul>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
